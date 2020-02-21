208ad90d42daa130a138368e9a6bcb2f3bde0ca1
Mohammad Abolfaz <duniandewon@gmail.com>
2020-02-21 17:01:37 +0700

    Add search logs functionality

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -6,7 +6,8 @@ import {
   DELETE_LOG,
   SET_CURRENT,
   CLEAR_CURRENT,
-  UPDATE_LOG
+  UPDATE_LOG,
+  SEARCH_LOGS
 } from './types';
 
 /** Get logs */
@@ -113,6 +114,26 @@ export const setCurrent = current => ({
 export const clearCurrent = () => ({
   type: CLEAR_CURRENT
 });
+
+/** Search logs */
+export const searchLogs = text => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch(`/logs?q=${text}`);
+    const data = await res.json();
+
+    dispatch({
+      type: SEARCH_LOGS,
+      payload: data
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data
+    });
+  }
+};
 
 export const setLoading = () => ({
   type: SET_LOADING
