49d5992701397d478dd0231e72b847cb69477f3c
Mohammad Abolfaz <duniandewon@gmail.com>
2020-02-21 09:37:49 +0700

    Add delete log functionality

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,5 +1,12 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from './types';
+import {
+  GET_LOGS,
+  SET_LOADING,
+  LOGS_ERROR,
+  ADD_LOG,
+  DELETE_LOG
+} from './types';
 
+/** Get logs */
 export const getLogs = () => async dispatch => {
   try {
     setLoading();
@@ -36,6 +43,27 @@ export const addLogs = log => async dispatch => {
     dispatch({
       type: ADD_LOG,
       payload: data
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data
+    });
+  }
+};
+
+/**Delete logs */
+export const deleteLogs = id => async dispatch => {
+  try {
+    setLoading();
+
+    await fetch(`/logs/${id}`, {
+      method: 'DELETE'
+    });
+
+    dispatch({
+      type: DELETE_LOG,
+      payload: id
     });
   } catch (err) {
     dispatch({
